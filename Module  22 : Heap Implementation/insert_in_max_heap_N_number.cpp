769a35fc4b966432e6a665836736d739f4e5f7e3
Md. Uzzal Mia <129536126+uzzal2200@users.noreply.github.com>
2024-06-16 15:59:12 +0600

    Add files via upload

---
+++ Module  22 : Heap Implementation/insert_in_max_heap_N_number.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //vector<int>v={50,40,45,30,35,42,32,25,20,10};
+     vector<int>v;
+   int n;
+   cin>>n;
+   for(int i=0;i<n;i++)
+  {
+   
+      int x;
+    cin>>x;
+    v.push_back(x);
+    int current_index=v.size() - 1;
+     //cout<<current_index<<endl;
+    
+    // cout<<parent_index<<endl;
+    while(current_index != 0)
+    {
+     int parent_index=(current_index - 1) /2;
+    if(v[current_index] > v[parent_index])
+    {
+        swap(v[parent_index],v[current_index] );
+
+    }
+     else 
+     break;
+     current_index=parent_index;
+    }
+  }
+    for(int val: v)
+    {
+        cout<<val<<" ";
+    }
+    return 0;
+}
