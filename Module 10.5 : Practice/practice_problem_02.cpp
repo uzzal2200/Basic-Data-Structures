61dc21e0e6351b7d823d3fa86ffb4d457e92474a
Md. Uzzal Mia <129536126+uzzal2200@users.noreply.github.com>
2024-06-16 16:05:28 +0600

    Add files via upload

---
+++ Module 10.5 : Practice/practice_problem_02.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+  public:
+  int val;
+    Node* next;
+    Node * prev;
+    Node(int val)
+    {
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+
+};
+void print_normal(Node *head)
+{
+    Node * temp=head;
+    while(temp != NULL)
+    {
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+}
+void insert_tail(Node *&head,Node *&tail,int val)
+{
+    Node * newNode=new Node(val);
+    if(tail == NULL)
+    {
+       head=newNode;
+       tail=newNode;
+    }
+    tail->next=newNode;
+    newNode->prev=tail;
+    tail=newNode;
+}
+void reverse(Node * head,Node * tail)
+{
+    Node * i=head;
+    Node * j=tail;
+    while(i != j && i->next != j)
+    {
+        swap(i->val,j->val);
+        i=i->next;
+        j=j->prev;
+    }
+    swap(i->val,j->val);
+}
+int main()
+{
+    Node * head=NULL;
+    Node * tail=NULL;
+    int val;
+    while(true)
+    {
+        cin>>val;
+        if(val == -1) break;
+         insert_tail(head,tail,val);
+    }
+     reverse(head,tail);
+    print_normal(head);
+    return 0;
+}
